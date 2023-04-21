63e02a7aba554ab812b6a9e7010144fbaa290560
Genyus <genyus@gmail.com>
2023-04-21 19:44:25 +0100

    fix: sets correct plugin details

--- web/app/plugins/cbf-academy/cbf-academy.php
+++ web/app/plugins/cbf-academy/cbf-academy.php
@@ -7,17 +7,17 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              http://example.com
+ * @link              https://codingblackfemales.com
  * @since             1.0.0
  * @package           CBFAcademy
  *
  * @wordpress-plugin
- * Plugin Name: WordPress Plugin Boilerplate
- * Plugin URI:  http://example.com/cbf-academy-uri/
- * Description: This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Plugin Name: CBF Academy
+ * Plugin URI:  https://codingblackfemales.com
+ * Description: Site-specific plugin for CBF Academy
  * Version:     1.0.0
  * Author:      Coding Black Females
- * Author URI:  http://example.com/
+ * Author URI:  https://codingblackfemales.com
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: cbf-academy
