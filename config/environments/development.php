a212e5c550fa082fa4aaf03dd2c1c9bf4277e4cc
Genyus <genyus@gmail.com>
2023-04-22 21:38:44 +0100

    fix: prevents debug output when using WP-CLI

--- config/environments/development.php
+++ config/environments/development.php
@@ -7,7 +7,7 @@ use Roots\WPConfig\Config;
 use function Env\env;
 
 Config::define('SAVEQUERIES', true);
-Config::define('WP_DEBUG', true);
+Config::define('WP_DEBUG', !(defined( 'WP_CLI' ) && WP_CLI));
 Config::define('WP_DEBUG_DISPLAY',  env('WP_DEBUG_DISPLAY') ?? true);
 Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? true);
 Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
