bcb067b7949d4abc23f517ce01edcbb8cd81f3b5
Genyus <genyus@gmail.com>
2023-05-09 21:42:44 +0100

    chore: test correctly for installed CA Replace verify-cert with find-certificate to check for installed cert Display notification if CA is already installed

--- .lando/scripts/trust-ca.sh
+++ .lando/scripts/trust-ca.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+keychain='/Library/Keychains/System.keychain'
 # Add the certificate to the macos trust store
-security verify-cert -c ~/.lando/certs/lndo.site.pem > /dev/null 2>&1
+security find-certificate -c "Lando Local CA" $keychain > /dev/null 2>&1
 if [ $? != 0 ]; then
-    echo "Adding certificate to trust store ..."
-    sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ~/.lando/certs/lndo.site.pem
+    echo "Adding Lando CA to keychain..."
+    sudo security add-trusted-cert -d -r trustRoot -k $keychain ~/.lando/certs/lndo.site.pem
+else
+    echo "Lando CA already exists, nothing to do here."
 fi
