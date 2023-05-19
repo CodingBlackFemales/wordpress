7cb8ca9cb6d970713eb9eebb8059503c52d5e016
Genyus <genyus@gmail.com>
2023-05-19 14:52:02 +0100

    refactor: relocate dev environment config variable

--- config/environments/development.php
+++ config/environments/development.php
@@ -6,15 +6,22 @@
 use Roots\WPConfig\Config;
 use function Env\env;
 
-Config::define('SAVEQUERIES', true);
-Config::define('WP_DEBUG', !(defined( 'WP_CLI' ) && WP_CLI));
-Config::define('WP_DEBUG_DISPLAY',  env('WP_DEBUG_DISPLAY') ?? true);
-Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? true);
-Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
-Config::define('SCRIPT_DEBUG', true);
-Config::define('DISALLOW_INDEXING', true);
+Config::define( 'SAVEQUERIES', true );
+Config::define( 'WP_DEBUG', ! ( defined( 'WP_CLI' ) && WP_CLI ) );
+Config::define( 'WP_DEBUG_DISPLAY', env( 'WP_DEBUG_DISPLAY' ) ?? true );
+Config::define( 'WP_DEBUG_LOG', env( 'WP_DEBUG_LOG' ) ?? true );
+Config::define( 'WP_DISABLE_FATAL_ERROR_HANDLER', true );
+Config::define( 'SCRIPT_DEBUG', true );
+Config::define( 'DISALLOW_INDEXING', true );
 
-ini_set('display_errors', '1');
+ini_set( 'display_errors', '1' );
 
 // Enable plugin and theme updates and installation from the admin
-Config::define('DISALLOW_FILE_MODS', false);
+Config::define( 'DISALLOW_FILE_MODS', false );
+
+/**
+ * Defines custom DB_HOST value when run outside container
+ */
+if ( defined( 'WP_CLI' ) && WP_CLI && ! env( 'LANDO' ) ) {
+	Config::define( 'DB_HOST', env( 'DB_HOST_EXTERNAL' ) ?? Config::get( 'DB_HOST' ) );
+}
