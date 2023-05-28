f222574b4b968f5649ba963e02ede15ee386a95d
Genyus <genyus@gmail.com>
2023-05-28 04:54:27 +0100

    chore: update sync script
    Add flags to skip database or asset synchronisation steps
    Implement asset synchronisation
    Add flag for local dev sync (no alias required in wp-cli.yml)

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -4,6 +4,44 @@
 # Version 1.1.0
 # Copyright (c) Ben Word
 
+LOCAL=false
+SKIP_DB=false
+SKIP_ASSETS=false
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --skip-db)
+      SKIP_DB=true
+      shift
+      ;;
+    --skip-assets)
+      SKIP_ASSETS=true
+      shift
+      ;;
+    --local)
+      LOCAL=true
+      shift
+      ;;
+    --*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1")
+      shift
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}"
+
+if [ $# != 2 ]
+then
+  echo "Usage: $0 [[--skip-db] [--skip-assets] [--local]] [ENV_FROM] [ENV_TO]"
+exit;
+fi
+
 FROM=$1
 TO=$2
 
@@ -18,16 +56,21 @@ declare -A DEV=(
 	["rootdomain"]="codingblackfemales.lndo.site"
 	["domain"]="wp.codingblackfemales.lndo.site"
 	["url"]="https://wp.codingblackfemales.lndo.site"
+	["dir"]="web/app/uploads/"
 )
 declare -A STAGING=(
 	["rootdomain"]="staging.codingblackfemales.com"
 	["domain"]="staging.codingblackfemales.com"
 	["url"]="https://staging.codingblackfemales.com"
+	["dir"]="codingblackfemales.com@ssh.gb.stackcp.com:/home/virtual/vps-da309d/e/e7eeed1b7b/staging_html/web/app/uploads/"
+	["port"]="22"
 )
 declare -A PRODUCTION=(
 	["rootdomain"]="codingblackfemales.com"
 	["domain"]="wp.codingblackfemales.com"
 	["url"]="https://wp.codingblackfemales.com"
+	["dir"]="codingblackfemales.com@ssh.gb.stackcp.com:/home/virtual/vps-da309d/e/e7eeed1b7b/public_html/web/app/uploads/"
+	["port"]="22"
 )
 
 case "$FROM-$TO" in
@@ -37,7 +80,7 @@ case "$FROM-$TO" in
 	development-staging)    DIR="up ⬆️ "; ;;
 	production-staging)     DIR="horizontally ↔️ "; ;;
 	staging-production)     DIR="horizontally ↔️ "; ;;
-	*) echo "usage: $0 production development | staging development | development staging | staging production | production staging" && exit 1 ;;
+	*) echo "usage: $0 [[--skip-db] [--skip-assets] [--local]] production development | staging development | development staging | staging production | production staging" && exit 1 ;;
 esac
 
 case "$FROM" in
@@ -52,9 +95,27 @@ case "$TO" in
 	staging)     DEST=("${(@fkv)STAGING}"); ;;
 esac
 
+if [ "$SKIP_DB" = false ]
+then
+  DB_MESSAGE=" - ${bold}reset the $TO database${normal} (${DEST[url]})"
+fi
+
+if [ "$SKIP_ASSETS" = false ]
+then
+  ASSETS_MESSAGE=" - sync ${bold}$DIR${normal} from $FROM (${SOURCE[url]})?"
+fi
+
+if [ "$SKIP_DB" = true ] && [ "$SKIP_ASSETS" = true ]
+then
+  echo "Nothing to synchronize."
+  exit;
+fi
+
 read "response?
-🔄  Would you really like to ⚠️  ${bold}reset the $TO database${normal} (${DEST[url]})
-    and sync ${bold}$DIR${normal} from $FROM (${SOURCE[url]})? [y/N] "
+🔄  Would you really like to
+${DB_MESSAGE}
+${ASSETS_MESSAGE}
+[y/N] "
 
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
 	# Change to site directory
@@ -78,7 +139,12 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
 	# Make sure both environments are available before we continue
 	availfrom() {
 		local AVAILFROM
-		AVAILFROM=$(wp "@$FROM" option get home 2>&1)
+
+		if [[ "$LOCAL" = true && $FROM == "development" ]]; then
+			AVAILFROM=$(wp option get home 2>&1)
+		else
+			AVAILFROM=$(wp "@$FROM" option get home 2>&1)
+		fi
 		if [[ $AVAILFROM == *"Error"* ]]; then
 			echo "❌  Unable to connect to $FROM"
 			exit 1
@@ -89,9 +155,14 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
 
 	availto() {
 		local AVAILTO
-		AVAILTO=$(wp "@$TO" option get home 2>&1)
+		if [[ "$LOCAL" = true && $TO == "development" ]]; then
+			AVAILTO=$(wp option get home 2>&1)
+		else
+			AVAILTO=$(wp "@$TO" option get home 2>&1)
+		fi
+
 		if [[ $AVAILTO == *"Error"* ]]; then
-			echo "❌  Unable to connect to $TO"
+			echo "❌  Unable to connect to $TO $AVAILTO"
 			exit 1
 		else
 			echo "✅  Able to connect to $TO"
@@ -99,17 +170,38 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
 	};
 
 	sync_db() {
+		if [ "$SKIP_DB" = true ]
+		then
+			return
+		fi
+
 		local DESTDOMAIN
 		local DESTPATH
 		local DESTSUBSITE
 		local SOURCESUBSITE
+		local EXPORTFILE
 
-		echo
+		echo "Syncing database..."
+		EXPORTFILE="data/export-$(date +'%Y%m%d%H%M%S').sql"
 
 		# Export/import database
 		wp "@$TO" db export "data/export-$(date +'%Y%m%d%H%M%S').sql" &&
 		wp "@$TO" db reset --yes &&
 		wp "@$FROM" db export - | wp "@$TO" db import -
+		# Export/import database, run search & replace
+		if [[ "$LOCAL" = true && $TO == "development" ]]; then
+			wp db export $EXPORTFILE --default-character-set=utf8mb4 &&
+			wp db reset --yes &&
+			wp "@$FROM" db export --default-character-set=utf8mb4 - | wp db import -
+		elif [[ "$LOCAL" = true && $FROM == "development" ]]; then
+			wp "@$TO" db export $EXPORTFILE --default-character-set=utf8mb4 &&
+			wp "@$TO" db reset --yes &&
+			wp db export --default-character-set=utf8mb4 - | wp "@$TO" db import -
+		else
+			wp "@$TO" db export $EXPORTFILE --default-character-set=utf8mb4 &&
+			wp "@$TO" db reset --yes &&
+			wp "@$FROM" db export --default-character-set=utf8mb4 - | wp "@$TO" db import -
+		fi
 
 		if [ $? -ne 0 ]; then
 			echo "❌  Database import failed" >&2
@@ -137,8 +229,7 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
 			echo
 			echo "Replacing $SOURCESUBSITE (sub-site) with $DESTSUBSITE"
 			wp @$TO db query "UPDATE wp_blogs SET domain='$DESTDOMAIN', path='$DESTPATH' WHERE domain='$SOURCESUBSITE';" &&
-			wp @$TO search-replace "$SOURCESUBSITE" "$DESTSUBSITE" --url="$DESTSUBSITE" &&
-			wp @$TO search-replace "$SOURCESUBSITE" "$DESTSUBSITE" --url="${SOURCE[url]}"
+			wp @$TO search-replace "$SOURCESUBSITE" "$DESTSUBSITE" --all-tables-with-prefix
 		done
 
 		# Run search & replace for primary domain
@@ -149,17 +240,25 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
 	};
 
 	sync_uploads() {
+		if [ "$SKIP_ASSETS" = true ]
+		then
+			return
+		fi
+
+		echo "Syncing assets $DIR from ${SOURCE[dir]} to ${DEST[dir]}..."
 		# Sync uploads directory
 		chmod -R 755 web/app/uploads/ &&
 		if [[ $DIR == "horizontally"* ]]; then
-			[[ $FROMDIR =~ ^(.*): ]] && FROMHOST=${BASH_REMATCH[1]}
-			[[ $FROMDIR =~ ^(.*):(.*)$ ]] && FROMDIR=${BASH_REMATCH[2]}
-			[[ $TODIR =~ ^(.*): ]] && TOHOST=${BASH_REMATCH[1]}
-			[[ $TODIR =~ ^(.*):(.*)$ ]] && TODIR=${BASH_REMATCH[2]}
+			[[ ${SOURCE[dir]} =~ ^(.*): ]] && FROMHOST=${BASH_REMATCH[1]}
+			[[ ${SOURCE[dir]} =~ ^(.*):(.*)$ ]] && FROMDIR=${BASH_REMATCH[2]}
+			[[ ${DEST[dir]} =~ ^(.*): ]] && TOHOST=${BASH_REMATCH[1]}
+			[[ ${DEST[dir]} =~ ^(.*):(.*)$ ]] && TODIR=${BASH_REMATCH[2]}
 
-			ssh -o ForwardAgent=yes $FROMHOST "rsync -aze 'ssh -o StrictHostKeyChecking=no' --progress $FROMDIR $TOHOST:$TODIR"
+			ssh -p ${SOURCE[port]} -o ForwardAgent=yes $FROMHOST "rsync -aze 'ssh -o StrictHostKeyChecking=no -p ${DEST[port]}' --progress $FROMDIR $TOHOST:$TODIR"
+		elif [[ $DIR == "down"* ]]; then
+			rsync -chavzP -e "ssh -p ${SOURCE[port]}" --progress "${SOURCE[dir]}" "${DEST[dir]}"
 		else
-			rsync -az --progress "$FROMDIR" "$TODIR"
+			rsync -az --progress "${SOURCE[dir]}" "${DEST[dir]}"
 		fi
 	};
 
@@ -177,7 +276,7 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
 	availfrom
 	availto
 	sync_db
-	# sync_uploads
+	sync_uploads
 	# notify
 
 	popd
