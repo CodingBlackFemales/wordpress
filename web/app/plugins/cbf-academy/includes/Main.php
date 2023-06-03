759971f88c7413c8c9956ea1bdedbf14fb67954e
Genyus <genyus@gmail.com>
2023-06-03 17:28:33 +0100

    fix: restore BuddyPages access for site admins
    Restore permissions lost for site admins in multisite

--- web/app/plugins/cbf-academy/includes/Main.php
+++ web/app/plugins/cbf-academy/includes/Main.php
@@ -10,7 +10,7 @@ namespace CBFAcademy;
 
 use CBFAcademy\Admin\Main as Admin;
 use CBFAcademy\Front\Main as Front;
-
+use CBFAcademy\Customizations\BuddyBoss;
 
 /**
  * Base Plugin class holding generic functionality
@@ -84,6 +84,7 @@ final class Main {
 
 		// Common includes.
 		Block::hooks();
+		BuddyBoss::hooks();
 
 		// Set up localisation.
 		self::load_plugin_textdomain();
