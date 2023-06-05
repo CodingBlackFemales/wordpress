b63769f96e2489f76e0676072734bdf390a96d16
Genyus <genyus@gmail.com>
2023-06-05 22:50:50 +0100

    fix: restore unfiltered_html capability for admins
    Restore capability removed for multisite admins

--- web/app/plugins/cbf-jobs/includes/Customizations/Capabilities.php
+++ web/app/plugins/cbf-jobs/includes/Customizations/Capabilities.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Ninja Forms Hooks
+ * Capability management
  *
  * @package     CBFJobs/Customizations
  * @version     1.0.0
@@ -26,18 +26,18 @@ class Capabilities {
 	 * @return void
 	 */
 	public static function hooks() {
-		add_action( 'init', array( __CLASS__, 'custom_grant_manage_network_users_capability' ), 11 );
+		add_action( 'init', array( __CLASS__, 'restore_admin_role_capabilities' ), 11 );
 	}
 
 	/**
-	 * Hooks onto ninja_forms_post_run_action_type_redirect to correctly decode redirection URL argument
-	 * Fixes URL provided when logging in from Discourse
+	 * Restores capabilities for multisite admin role
 	 */
-	public static function custom_grant_manage_network_users_capability() {
+	public static function restore_admin_role_capabilities() {
 		$role = get_role( 'administrator' );
 
 		if ( $role ) {
 			$role->add_cap( 'manage_network_users', true );
+			$role->add_cap( 'unfiltered_html' );
 		}
 	}
 }
