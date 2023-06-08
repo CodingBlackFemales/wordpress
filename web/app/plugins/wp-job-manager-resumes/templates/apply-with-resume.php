14a22d414f76bedcbd58a937ede900e53c42d0dd
Genyus <genyus@gmail.com>
2023-06-08 17:53:41 +0100

    chore(deps): update premium dependencies
    - Upgrade wp-job-manager-alerts (1.5.6 => 1.6.0)
    - Upgrade wp-job-manager-resumes (1.18.6 =>
    1.19.0)
    - Upgrade wp-job-manager-wc-paid-listings (2.9.8 => 2.9.9)

--- web/app/plugins/wp-job-manager-resumes/templates/apply-with-resume.php
+++ web/app/plugins/wp-job-manager-resumes/templates/apply-with-resume.php
@@ -23,19 +23,19 @@ if ( ! get_option( 'resume_manager_force_application' ) ) {
 
 if ( is_user_logged_in() && count( $resumes ) ) : ?>
 	<form class="apply_with_resume" method="post">
-		<p><?php _e( 'Apply using your online resume; just enter a short message to send your application.', 'wp-job-manager-resumes' ); ?></p>
+		<p><?php esc_html_e( 'Apply using your online resume; just enter a short message to send your application.', 'wp-job-manager-resumes' ); ?></p>
 		<p>
-			<label for="resume_id"><?php _e( 'Online resume', 'wp-job-manager-resumes' ); ?>:</label>
+			<label for="resume_id"><?php esc_html_e( 'Online resume', 'wp-job-manager-resumes' ); ?>:</label>
 			<select name="resume_id" id="resume_id" required>
 				<?php
 				foreach ( $resumes as $resume ) {
-					echo '<option value="' . absint( $resume->ID ) . '">' . esc_html( get_resume_select_label( $resume ) ) . '</option>';
+					echo '<option value="' . esc_attr( absint( $resume->ID ) ) . '">' . esc_html( get_resume_select_label( $resume ) ) . '</option>';
 				}
 				?>
 			</select>
 		</p>
 		<p>
-			<label><?php _e( 'Message', 'wp-job-manager-resumes' ); ?>:</label>
+			<label><?php esc_html_e( 'Message', 'wp-job-manager-resumes' ); ?>:</label>
 			<textarea name="application_message" cols="20" rows="4" required>
 			<?php
 			if ( isset( $_POST['application_message'] ) ) {
@@ -52,16 +52,16 @@ if ( is_user_logged_in() && count( $resumes ) ) : ?>
 		</p>
 		<p>
 			<input type="submit" name="wp_job_manager_resumes_apply_with_resume" value="<?php esc_attr_e( 'Send Application', 'wp-job-manager-resumes' ); ?>" />
-			<input type="hidden" name="job_id" value="<?php echo absint( $post->ID ); ?>" />
+			<input type="hidden" name="job_id" value="<?php echo esc_attr( absint( $post->ID ) ); ?>" />
 		</p>
 	</form>
 <?php else : ?>
-	<form class="apply_with_resume" method="post" action="<?php echo get_permalink( get_option( 'resume_manager_submit_resume_form_page_id' ) ); ?>">
-		<p><?php _e( 'You can apply to this job and others using your online resume. Click the link below to submit your online resume and email your application to this employer.', 'wp-job-manager-resumes' ); ?></p>
+	<form class="apply_with_resume" method="post" action="<?php echo esc_url( get_permalink( get_option( 'resume_manager_submit_resume_form_page_id' ) ) ); ?>">
+		<p><?php esc_html_e( 'You can apply to this job and others using your online resume. Click the link below to submit your online resume and email your application to this employer.', 'wp-job-manager-resumes' ); ?></p>
 
 		<p>
 			<input type="submit" name="wp_job_manager_resumes_apply_with_resume_create" value="<?php esc_attr_e( 'Submit Resume &amp; Apply', 'wp-job-manager-resumes' ); ?>" />
-			<input type="hidden" name="job_id" value="<?php echo absint( $post->ID ); ?>" />
+			<input type="hidden" name="job_id" value="<?php echo esc_attr( absint( $post->ID ) ); ?>" />
 		</p>
 	</form>
 <?php endif; ?>
