c27787d932122e0a1bdecde9b1ca9055ce6f0bfc
Genyus <genyus@gmail.com>
2023-07-24 17:38:21 +0100

    chore: fix script errors
    Set correct variable reference
    Add missing environment alias

--- scripts/update-quizzes.sh
+++ scripts/update-quizzes.sh
@@ -12,9 +12,9 @@ url=($(wp @$environment site list --blog_id=${blog_id} --field=url | sed -E 's|h
 post_id_list=($(wp @$environment post list --post_type=sfwd-quiz --field=ID --url=${url}))
 
 echo "Updating quizzes in $environment environment"
-wp @$environment db query "UPDATE wp_${blogid}_learndash_pro_quiz_master SET disabled_answer_mark = 1"
+wp @$environment db query "UPDATE wp_${blog_id}_learndash_pro_quiz_master SET disabled_answer_mark = 1"
 
 for post_id in ${post_id_list[@]}; do
 	echo "Updating quiz ID:${post_id}"
-	wp post meta patch update ${post_id} _sfwd-quiz sfwd-quiz_disabledAnswerMark true --url=${url} --format=json
+	wp @$environment post meta patch update ${post_id} _sfwd-quiz sfwd-quiz_disabledAnswerMark true --url=${url} --format=json
 done
