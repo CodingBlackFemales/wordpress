b84d8003d5cc89b14e33757ac181d3077ade1ae3
Genyus <genyus@gmail.com>
2023-07-24 16:48:56 +0100

    chore: add script to update quiz settings
    Disables correct/incorrect answer marks for all quiz results pages
    
    Closes https://codingblackfemales.atlassian.net/browse/BCMP-239

---
+++ scripts/update-quizzes.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# != 1 ]
+then
+	environment='development'
+else
+	environment=$1
+fi
+
+blog_id=2
+url=($(wp @$environment site list --blog_id=${blog_id} --field=url | sed -E 's|https?://||;s|/$||'))
+post_id_list=($(wp @$environment post list --post_type=sfwd-quiz --field=ID --url=${url}))
+
+echo "Updating quizzes in $environment environment"
+# wp @$environment db query "UPDATE wp_${blogid}_learndash_pro_quiz_master SET disabled_answer_mark = 1"
+
+for post_id in ${post_id_list[@]}; do
+	echo "Updating quiz ID:${post_id}"
+	# wp post meta patch update ${post_id} _sfwd-quiz sfwd-quiz_disabledAnswerMark true --url=${url} --format=json
+done
