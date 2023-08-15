51633e3daf6f4f0b7f2717ee882ae9c8d7be1c83
Genyus <genyus@gmail.com>
2023-08-15 02:00:21 +0100

    chore: improve logging

--- web/app/plugins/cbf-multisite/includes/Customizations/WP_Cron.php
+++ web/app/plugins/cbf-multisite/includes/Customizations/WP_Cron.php
@@ -24,6 +24,7 @@ class WP_Cron {
 	public static function export_quiz_activity() {
 		// Only attempt if Learndash is installed
 		if ( defined( 'LEARNDASH_VERSION' ) ) {
+			error_log( 'Exported quiz results' );
 			$results = LearnDash::get_results();
 			Airtable::insert_quiz_activities( $results );
 		}
