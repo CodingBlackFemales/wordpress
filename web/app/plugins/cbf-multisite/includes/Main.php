1da0c78a4bf7d3471018e4f179892dc5147b1c28
Genyus <genyus@gmail.com>
2023-08-15 02:46:07 +0100

    feat: adds `list` subcommand
    Improves PHPDoc comments

--- web/app/plugins/cbf-multisite/includes/Main.php
+++ web/app/plugins/cbf-multisite/includes/Main.php
@@ -10,8 +10,7 @@ namespace CodingBlackFemales\Multisite;
 
 use CodingBlackFemales\Multisite\Admin\Main as Admin;
 use CodingBlackFemales\Multisite\Front\Main as Front;
-use CodingBlackFemales\Multisite\Customizations\Universal as Universal;
-use CodingBlackFemales\Multisite\Customizations\WP_CLI_Command as WP_CLI_Command;
+use CodingBlackFemales\Multisite\Customizations\Quiz_Results_Command as Quiz_Results_Command;
 use CodingBlackFemales\Multisite\Customizations\WP_Cron as WP_Cron;
 
 
@@ -88,7 +87,7 @@ final class Main {
 		}
 
 		if ( Utils::is_request( 'cli' ) ) {
-			WP_CLI_Command::hooks();
+			Quiz_Results_Command::hooks();
 		}
 
 		// Common includes.
