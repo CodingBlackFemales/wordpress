85dd9ddcc9bec541a749eaa175a70d7e992b02c8
Genyus <genyus@gmail.com>
2023-08-15 02:46:07 +0100

    feat: adds `list` subcommand
    Improves PHPDoc comments

--- web/app/plugins/cbf-multisite/includes/Customizations/Quiz_Results_Command.php
+++ web/app/plugins/cbf-multisite/includes/Customizations/Quiz_Results_Command.php
@@ -14,14 +14,28 @@ if ( ! defined( 'ABSPATH' ) || ! defined( 'WP_CLI' ) ) {
 }
 
 /**
- * Custom WP-CLI integration class.
- */
-/**
- * Just a few sample commands to learn how WP-CLI works
+ * Retrieves and exports Learndash quiz results to Airtable.
+ *
+ * ## EXAMPLES
+ *
+ *     # List all quiz results
+ *     $ wp cbf quiz_results list
+ *     Success: Retrieved 12 quiz activities from Learndash.
+ *
+ *     # Export recent quiz results to Airtable
+ *     $ wp cbf quiz_results export
+ *     Success: Exported 12 quiz activities to Airtable.
  */
 class Quiz_Results_Command extends \WP_CLI_Command {
 	/**
-	 * Exports quiz results to Airtable.
+	 * Exports Learndash quiz results to Airtable.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     # Export recent quiz results to Airtable
+	 *     $ wp cbf quiz_results export
+	 *     Success: Exported 12 quiz activities to Airtable.
+	 *
 	 * ## OPTIONS
 	 *
 	 * [--verbose]
@@ -33,26 +47,55 @@ class Quiz_Results_Command extends \WP_CLI_Command {
 		if ( ! empty( $args ) ) {
 			WP_CLI::error( 'Command syntax: wp cbf export-quiz-results' );
 		} else {
-			$results = LearnDash::get_results();
+			$results = $this->get_results( $verbose, fn() => LearnDash::get_results() );
+			$responses = $this->get_results( $verbose, fn() => Airtable::insert_quiz_activities( $results ) );
 
-			if ( $verbose ) {
-				foreach ( $results as $result ) {
-					WP_CLI::line( print_r( $result ) );
-				}
-			}
-
-			WP_CLI::line( 'Retrieved ' . count( $results ) . ' quiz activities from Learndash.' );
-
-			$responses = Airtable::insert_quiz_activities( $results );
-
-			if ( $verbose ) {
-				foreach ( $responses as $response ) {
-					WP_CLI::line( print_r( $response ) );
-				}
-			}
-
-			WP_CLI::line( 'Inserted ' . count( $responses ) . ' quiz activities to Airtable.' );
+			WP_CLI::success( 'Exported ' . count( $responses ) . ' quiz activities to Airtable.' );
 		}
+	}
+
+	/**
+	 * Lists Learndash quiz results.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     # List all quiz results
+	 *     $ wp cbf quiz_results list
+	 *     Success: Retrieved 12 quiz activities from Learndash.
+	 *
+	 * ## OPTIONS
+	 *
+	 * [--verbose]
+	 * : Shows verbose output.
+	 *
+	 * @subcommand list
+	 */
+	public function list_( $args, $assoc_args ) {
+		$verbose = array_key_exists( 'verbose', $assoc_args );
+
+		if ( ! empty( $args ) ) {
+			WP_CLI::error( 'Command syntax: wp cbf export-quiz-results' );
+		} else {
+			$results = $this->get_results( $verbose, fn() => LearnDash::get_results() );
+
+			WP_CLI::success( 'Retrieved ' . count( $results ) . ' quiz activities from Learndash.' );
+		}
+	}
+
+	/**
+	 * Returns iterable results of a function call.
+	 * If verbose mode is enabled, iterates through array and displays each element
+	 */
+	protected function get_results( $verbose, callable $func ) {
+		$results = $func();
+
+		if ( $verbose ) {
+			foreach ( $results as $result ) {
+				WP_CLI::line( print_r( $result ) );
+			}
+		}
+
+		return $results;
 	}
 
 	/**
