b8e394b437e50dd46b628eba0b4d5d14572cb6cc
Genyus <genyus@gmail.com>
2024-11-23 00:58:59 +0000

    chore(deps): adds Jetpack rules

---
+++ web/app/jetpack-waf/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+define( 'DISABLE_JETPACK_WAF', false );
+if ( defined( 'DISABLE_JETPACK_WAF' ) && DISABLE_JETPACK_WAF ) return;
+define( 'JETPACK_WAF_MODE', 'silent' );
+define( 'JETPACK_WAF_SHARE_DATA', false );
+define( 'JETPACK_WAF_SHARE_DEBUG_DATA', false );
+define( 'JETPACK_WAF_DIR', '/app/web/app/jetpack-waf' );
+define( 'JETPACK_WAF_WPCONFIG', '/app/web/app/../wp-config.php' );
+require_once '/app/web/app/plugins/jetpack/vendor/autoload.php';
+Automattic\Jetpack\Waf\Waf_Runner::initialize();
