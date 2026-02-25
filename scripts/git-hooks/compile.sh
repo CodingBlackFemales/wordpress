ad821109e8bc5da21d598a62d1dfe3bd2f80c44e
Genyus <genyus@gmail.com>
2026-02-25 15:29:06 -0400

    ci: add hosting env hooks
    - Add script to trigger `composer install` after git pull or checkout

---
+++ scripts/git-hooks/compile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+changedFiles="$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)"
+
+runOnChange() {
+	echo "$changedFiles" | grep -q "$1" && eval "$2"
+}
+
+runOnChange composer.lock "composer install --no-dev --prefer-dist --no-interaction --no-progress --optimize-autoloader"
