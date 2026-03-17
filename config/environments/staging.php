fedd21a42926ff95ba69c6b91ea7ad9186462bd4
Genyus <genyus@gmail.com>
2026-03-17 15:03:23 -0400

    chore: enable debugging in staging

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -16,3 +16,7 @@ use Roots\WPConfig\Config;
 
 Config::define( 'DISALLOW_INDEXING', true );
 Config::define( 'SUBDOMAIN_INSTALL', false );
+Config::define( 'WP_DEBUG', ( env( 'WP_DEBUG' ) ?? false ) && ! ( defined( 'WP_CLI' ) && WP_CLI ) );
+Config::define( 'WP_DEBUG_DISPLAY', env( 'WP_DEBUG_DISPLAY' ) ?? false );
+Config::define( 'WP_DEBUG_LOG', env( 'WP_DEBUG_LOG' ) ?? false );
+Config::define( 'WP_DISABLE_FATAL_ERROR_HANDLER', false );
