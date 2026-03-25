b77771f9e8289ca952f7529486ef9ce6a917532b
Genyus <genyus@gmail.com>
2026-03-24 23:11:43 -0400

    chore(deps): remove WP All Import Pro plugin

--- web/app/plugins/wp-all-import-pro/addon-api/templates/fields/unsupported.php
+++
@@ -1,8 +0,0 @@
-<p>
-    <?php
-    printf(
-        __('The field <b>%s</b> is currently not supported. Please contact support for more information.', 'wp-all-import-pro'),
-        $field['type']
-    );
-    ?>
-</p>
--- web/app/plugins/wp-all-import-pro/addon-api/templates/separator.php
+++
@@ -1,3 +0,0 @@
-<div class="pmxi-addon-separator">
-    <div class="pmxi-addon-separator-content">
-        <h3 class="pmxi-addon-separator__title"><?php echo $field['label']; ?></h3>
