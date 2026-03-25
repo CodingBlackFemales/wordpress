244916d0abc55a3a909345e7300c80b8ecffaafa
Genyus <genyus@gmail.com>
2026-03-24 23:11:43 -0400

    chore(deps): remove WP All Import Pro plugin

--- web/app/plugins/wp-all-import-pro/addon-api/templates/fields/iconpicker.php
+++
@@ -1,1 +0,0 @@
-<input type="text" placeholder="" value="<?php echo esc_attr($field_value); ?>" name="<?php echo esc_attr($html_name); ?>" data-test="input" class="text widefat rad4" style="width:200px;" />
--- web/app/plugins/wp-all-import-pro/addon-api/templates/fields/user.php
+++
@@ -1,1 +0,0 @@
-<input type="text" value="<?php echo esc_attr($field_value); ?>" data-test="input" name="<?php echo esc_attr($html_name); ?>" placeholder="<?php echo esc_attr($html_placeholder); ?>" class="text widefat rad4" />
