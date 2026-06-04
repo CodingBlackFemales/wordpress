ffd9b0137c73aab781bebe472c59a7c15f6fcfa7
Genyus <genyus@gmail.com>
2026-06-04 00:05:54 -0400

    chore: make skills check export daily

--- web/app/plugins/cbf-multisite/includes/Customizations/WP_Cron.php
+++ web/app/plugins/cbf-multisite/includes/Customizations/WP_Cron.php
@@ -31,30 +31,14 @@ class WP_Cron {
 	}
 
 	/**
-	 * Add a weekly schedule.
-	 */
-	public static function extend_cron_schedules( $schedules ) {
-		if ( ! array_key_exists( 'weekly', $schedules ) ) {
-			$schedules['weekly'] = array(
-				'interval' => 604800,
-				'display' => __( 'Once Weekly' ),
-			);
-		}
-
-		return $schedules;
-	}
-
-	/**
 	 * Hook in methods.
 	 */
 	public static function hooks() {
-		add_filter( 'cron_schedules', array( __CLASS__, 'extend_cron_schedules' ) );
-
 		if ( defined( 'ENABLE_CBF_SCHEDULED_EXPORT' ) && ENABLE_CBF_SCHEDULED_EXPORT && get_current_blog_id() === intval( ACADEMY_SITE_ID ) ) {
 			add_action( self::EXPORT_EVENT_NAME, array( __CLASS__, 'export_quiz_activity' ) );
 
 			if ( ! wp_next_scheduled( self::EXPORT_EVENT_NAME ) ) {
-				wp_schedule_event( strtotime( 'this Saturday 8:00am', time() ), 'weekly', WP_Cron::EXPORT_EVENT_NAME );
+				wp_schedule_event( strtotime( 'today 8:00am', time() ), 'daily', WP_Cron::EXPORT_EVENT_NAME );
 			}
 		}
 	}
